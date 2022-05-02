18886fd1b96b17fcbd5c9d1a084ce67ed3ed635d
Ukobaandrew <101108667+Ukobaandrew@users.noreply.github.com>
2022-05-02 20:49:33 +0100

    created a closing program

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // to create a new document
-document.getElementsByClassName("plus");
-document.createAttribute(x);
+const close = document.getElementsByClassName("trash");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].addEventListener("click", function () {
+    this.parentElement.style.display = "none";
+  });
+}
+
+// create a new document when a new document is added and button is clicked
+
